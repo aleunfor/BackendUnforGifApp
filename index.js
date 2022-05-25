41d8c7d6f7c8313690e8a84bc67d245b964b636e
Alejandro Hermosilla <51329486+aleunfor@users.noreply.github.com>
2022-05-25 18:15:13 -0400

    Upload App

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv').config()
+
+const dashboadRoutes = require('./routes/dashboard');
+const verifyToken = require('./routes/validate-token');
+
+const app = express();
+
+// capturar body
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// Conexión a Base de datos
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.DBNAME}.zpxsb.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+    .then(() => console.log('DB Connected'))
+    .catch(e => console.log('DB Error:', e))
+
+// import routes
+const authRoutes = require('./routes/auth');
+
+// route middlewares
+app.get('/', (req, res) => {
+    res.json({
+        estado: true,
+        mensaje: 'It works!'
+    })
+});
+app.use('/api/user', authRoutes);
+app.use('/api/dashboard', verifyToken, dashboadRoutes);
+
+// iniciar server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`)
+})
